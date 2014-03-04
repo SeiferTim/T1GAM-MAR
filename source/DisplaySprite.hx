510e48d5777ffeb4e6404b8282d043c5ae48a749
SeiferTim <seifertim@gmail.com>
2014-03-04 13:36:12 -0600

    Just getting started...

---
+++ source/DisplaySprite.hx
@@ -0,0 +1,37 @@
+package ;
+
+import flixel.addons.display.FlxNestedSprite;
+
+class DisplaySprite extends FlxNestedSprite
+{
+
+	private var _z:Float = 0;
+	
+	/**
+	 * If the system should calculate Z or not
+	 */
+	public var calcZ:Bool = true;
+	
+	public function new(X:Float=0, Y:Float=0) 
+	{
+		super(X, Y);
+	}
+	
+	function get_z():Float
+	{
+		if (calcZ)
+			return y + height;
+		else
+			return _z;
+	}
+	
+	function set_z(Value:Float):Float
+	{
+		if (!calcZ)
+			_z = Value;
+		return _z;
+	}
+	
+	public var z(get_z, set_z):Float;
+	
+}
---
+++ source/GameFont.hx
@@ -0,0 +1,14 @@
+package ;
+
+import flixel.FlxSprite;
+
+class GameFont extends FlxSprite
+{
+
+	public function new(X:Float=0, Y:Float=0, ?SimpleGraphic:Dynamic) 
+	{
+		super(X, Y, ?SimpleGraphic);
+		
+	}
+	
+}
